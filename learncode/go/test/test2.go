aa13395ba420900283369eacb346a02bcb171486
chenpingrobinth <chenpingrobinth@didichuxing.com>
2018-07-03 14:46:20 +0800

    add wiki

---
+++ learncode/go/test/test2.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+// fmt 打印指针内容
+
+type A struct {
+	a int
+	b int
+}
+
+type B struct {
+	c int
+	d int
+	info *A
+}
+
+func main() {
+	b := &B{
+		3,
+		4,
+		&A{
+			1,
+			2,
+		},
+	}
+
+	//指针里面嵌套指针，第二个指针无法打印
+	fmt.Printf("%v", b)
+}
